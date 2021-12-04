aa23941d57d63959ee002f3485e7a52b07abf199
EnesBasic <ssamss3004@gmail.com>
2021-12-04 11:40:49 +0100

    init

---
+++ 26_11/index.php
@@ -0,0 +1,18 @@
+<?php
+#$nizGradova=("BiH"=>"Sarajevo", "Hrvatska" => "Mostar", "Srbija" => "Beograd");
+$vrijednost = 0;
+
+switch($vrijednost){
+    case 0:
+        echo "broj je jedan";
+    break;
+    case 1:
+        echo "broj je dva";
+        break;
+    default:
+        echo "broj nije ni nula, ni jedan, ni dva!";
+    break;
+    }
+
+
+?>
