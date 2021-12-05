38005e148a876425c1685b6b867817ba98293791
EnesBasic <ssamss3004@gmail.com>
2021-12-05 16:31:21 +0100

    init

--- 26_11/zadaci 24.../get.php
+++ 26_11/zadaci 24.../get.php
@@ -1,3 +1,6 @@
 <?php
-    echo "Enes Basic studira " . $_GET['subject'] ." na " . $_GET['web'];
+    //echo "Enes Basic studira " . $_GET['subject'] ." na " . $_GET['web'];
+    echo "Enes Basic otvara " . $_GET['page'] ." na svojoj stranici";
 ?>
+
+<h3>---------------------------------1-verzija_kraj--------------------------------</h3>
