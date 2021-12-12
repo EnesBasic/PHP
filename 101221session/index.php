1dbb7af443faafeea5acf9f32b4c592d91848c4f
Enes Basic <ssamss3004@gmail.com>
2021-12-12 22:11:48 +0100

    init

--- 101221session/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-session_start();
-
-$_SESSION["ime_korisnika"]= "Enes";
-$_SESSION["prezime_korisnika"]= "Basic";
-$_SESSION["status_korisnika"]= "2";
-
-?>
--- 1012session/index1.php
+++ 1012session/index1.php
@@ -6,9 +6,13 @@ if (isset($_SESSION["ime_korisnika"])){
     echo "Ime: " .$_SESSION["ime_korisnika"];
 }
 
+echo "<br>";
+
 if (isset($_SESSION["prezime_korisnika"])){
     echo "Prezime: " .$_SESSION["prezime_korisnika"];
 }
+
+echo "<br>";
 
 if (isset($_SESSION["status_korisnika"])){
     echo "Status: " .$_SESSION["status_korisnika"];
