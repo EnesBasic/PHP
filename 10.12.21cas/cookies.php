a98d8404f8010c702a7b9d06680f37955d0e1ab2
Enes Basic <ssamss3004@gmail.com>
2021-12-12 21:16:20 +0100

    init

--- 10.12.21cas/cookies.php
+++ 10.12.21cas/cookies.php
@@ -1,5 +1,25 @@
 <?php
 //setcookie("ime","Enes");
-setcookie("ime","enes",time() + 60*60*24*30);
+setcookie("ime","ITAcademy7", time() + 60*60*24*30);
+//brisanje cookieja
+//setcookie("ime","enes",time()-3600);
+//echo "Vrijednost je :" . $_COOKIE["ime"];
+if(!$_COOKIE["ime"]=="ITAcademy6"){
+    header("refresh:0");
+}
 
+if (isset ($_COOKIE["ime"])){
+    echo ("Vrijednost je:" .$_COOKIE["ime"]);
+}else{
+    echo ("Ne postoji taj cookie!");
+}
+
+echo "<br>";
+
+if (count($_COOKIE)>0){
+echo "cookies are enabled";
+}else{
+    echo "cookies are disabled";
+}
 ?>
+
