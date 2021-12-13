6bd601292752f695af191b5331718dc37d1be00e
Enes Basic <ssamss3004@gmail.com>
2021-12-14 00:06:56 +0100

    init

--- 1012session/index.php
+++ 1012session/index.php
@@ -2,8 +2,8 @@
 
 session_start();
 
-$_SESSION["ime_korisnika"]= "Enes";
-$_SESSION["prezime_korisnika"]= "Basic";
-$_SESSION["status_korisnika"]= "2";
-
+$_SESSION["ime_korisnika"] = "Enes";
+$_SESSION["prezime_korisnika"] = "Basic";
+$_SESSION["status_korisnika"] = "2";
+ 
 ?>
--- 1012session/index1.php
+++ 1012session/index1.php
@@ -2,6 +2,23 @@
 
 session_start();
 
+function session_check($ispis, $kljuc){
+    if(isset($_SESSION[$kljuc])){
+       echo $ispis .": " . $_SESSION[$kljuc];
+       echo "<br>";
+    }
+}
+
+session_check("ime", "ime_korisnika");
+session_check("prezime", "prezime_korisnika");
+session_check("status", "status_korisnika");
+
+
+
+/**
+ * 
+Tada ovo vise ne treba:
+
 if (isset($_SESSION["ime_korisnika"])){
     echo "Ime: " .$_SESSION["ime_korisnika"];
 }
@@ -17,6 +34,7 @@ echo "<br>";
 if (isset($_SESSION["status_korisnika"])){
     echo "Status: " .$_SESSION["status_korisnika"];
 }
-
+}
+ */
 
 ?>
