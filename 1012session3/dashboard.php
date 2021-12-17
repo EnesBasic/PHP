b07c77b9b56ec15967e601a9b193ab2be847b8f8
EnesBasic <ssamss3004@gmail.com>
2021-12-17 15:09:48 +0100

    init

--- 1012session3/dashboard.php
+++ 1012session3/dashboard.php
@@ -5,7 +5,7 @@ session_start();
 function session_checker($kljuc){
     if (isset($_SESSION[$kljuc])){
         echo "Vrijednost 1:" .$_SESSION[$kljuc];
-        echo <br>;
+        echo "<br>";
     }
 }
 session_checker("username");
