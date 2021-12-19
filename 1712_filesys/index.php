4f7e08d4ce26a442f054d1137d236bee26a14874
EnesBasic <ssamss3004@gmail.com>
2021-12-19 19:49:46 +0100

    init

--- 1712_filesys/index.php
+++ 1712_filesys/index.php
@@ -3,7 +3,16 @@
 $text = "5678";
 file_put_contents("test.txt", $text);
 
-echo $_SERVER["DOCUMENT_ROOT"];
+$sadrzaj1 = "ovo je neka recenica";
+file_put_contents("proba1.txt", $sadrzaj1);
+
+$sadrzaj2 = 1234567890;
+file_put_contents("proba2.txt", $sadrzaj2);
+
+$sadrzaj3 = array("korisnik1", "korisnik2", "korisnik3");
+file_put_contents("proba3.txt", $sadrzaj3);
+
+//echo $_SERVER["DOCUMENT_ROOT"];
 
 
 
