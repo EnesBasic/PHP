6272286fd07a46e4ea8f968192b04473145557bd
Enes Basic <ssamss3004@gmail.com>
2021-12-23 21:05:12 +0100

    init

--- PHPMostar/index.php
+++ PHPMostar/index.php
@@ -7,6 +7,34 @@
     <title>Document</title>
 </head>
 <body>
+
+    <?php
+    if (isset($_POST["submit"])){
+        $user = $_POST["username"];
+        $pass = $_POST["password"];
+        if($user === "admin" && $pass === "1234"){
+        echo "welcome" ." " . $user;
+        echo " .Password and username matched!";
+        }else{
+            echo "error! please enter correct data!";
+        }
+    }
+
+    ?>
+
+</body>
+</html>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
     <form action="" method="post">
      <table align="center">
          <tr>
