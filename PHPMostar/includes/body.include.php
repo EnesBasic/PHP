21e821eb2c273fd0c296da9871d2753f65e1d910
Enes Basic <ssamss3004@gmail.com>
2021-12-23 22:15:29 +0100

    init

---
+++ PHPMostar/includes/body.include.php
@@ -0,0 +1,12 @@
+<body class="is-preload">
+		<div id="wrapper">
+			<div id="bg"></div>
+			<div id="overlay"></div>
+			<div id="main"></div>
+		</div>
+		<script>
+			window.onload = function() { document.body.classList.remove('is-preload'); }
+			window.ontouchmove = function() { return false; }
+			window.onorientationchange = function() { document.body.scrollTop = 0; }
+		</script>
+</body>
