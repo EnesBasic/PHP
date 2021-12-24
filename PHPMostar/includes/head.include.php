7c2df92fb87e4c3775b990aab9b8746d62011b22
Enes Basic <ssamss3004@gmail.com>
2021-12-24 19:02:59 +0100

    init

--- PHPMostar/includes/head.include.php
+++ PHPMostar/includes/head.include.php
@@ -1,7 +1,8 @@
 <head>
 	<title>Aerial by HTML5 UP</title>
 		<meta charset="utf-8" />
-		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no"/>
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
 		<link rel="stylesheet" href="assets/css/main.css" />
 		<noscript><link rel="stylesheet" href="assets/css/noscript.css" /></noscript>
 </head>
