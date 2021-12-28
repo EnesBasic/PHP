6ab9d9dc06eb7e91da642016f344e4a58cbac896
EnesBasic <ssamss3004@gmail.com>
2021-12-28 13:50:18 +0100

    init

--- PHPSa/register.php
+++ PHPSa/register.php
@@ -19,30 +19,34 @@
     </form>
 
     <?php
-        if(isset($_POST["username"])&& isset($_POST["password"])){
+        if(isset($_POST["username"]) && isset($_POST["password"])){
+            
             if(!empty($_POST["username"]) && !empty($_POST["password"])){
-
                 if(file_exists("users.json")){
-                    $users = file_get_contents("users.json");
+                    file_get_contents("users.json");
                     $users = json_decode($users, true);
-                    }else{
+                }else{
                     $users = array();
-                    }
+                }
 
                 $username = $_POST["username"];
                 $password = $_POST["password"];
 
-                $users[] = array ("username" => $username, "password" => $password);
-
+                $users[]=array("username" => $username, "password" => $password);
+                    
                 if(file_put_contents("users.json", json_encode($users))){
-                    header("Location: login.php?message=Successfully registered, please log in !");
+                    header("Location: login.php?message=Successfully registered, please log in!");
                 }else{
-                    echo "<span style = 'color: red;'>Error : File not saved!</span>";
+                    echo "<span style='color: red;'>ERROR wrong username or password!!!!</span>";
                 }
-            }else{
-                echo "<span style = 'color: red;'>You entered empty fields!</span>";
+
+        }else{
+                echo "<span style='color: red;'>You entered empty fields!</span>";
             }
         }
+
     ?>
+
 </body>
 </html>
+
