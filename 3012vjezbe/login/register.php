6461e6d78945c6690de64fc46ab5c4fa78525bf3
EnesBasic <ssamss3004@gmail.com>
2021-12-30 14:18:58 +0100

    init

--- 3012vjezbe/login/register.php
+++ 3012vjezbe/login/register.php
@@ -1,7 +1,4 @@
-
-<?php
- include('server.php');
-?>
+<?php include('server.php');?>
 
 <!DOCTYPE html>
 <html lang="en">
@@ -22,25 +19,27 @@
 
     </div>
     <form method="POST" action="register.php">
-        <div class="input-group">
-            <label>Username</label>
-            <input type="text" name="username">
-        </div>
-        <div class="input-group">
-            <label>Email</label>
-            <input type="text" name="email">
-        </div>
-        <div class="input-group">
-            <label>Password</label>
-            <input type="password" name="password_1">
-        </div>
-        <div class="input-group">
-            <label>Confirm Password</label>
-            <input type="password" name="password_2">
-        </div>
-        <div class="input-group">
-            <button type="submit" name="register" class="btn">Register</button>
-        </div>
+        <!-- display validations errors here -->
+        <?php include('errors.php'); ?>
+            <div class="input-group">
+                <label>Username</label>
+                <input type="text" name="username">
+            </div>
+            <div class="input-group">
+                <label>Email</label>
+                <input type="text" name="email">
+            </div>
+            <div class="input-group">
+                <label>Password</label>
+                <input type="password" name="password_1">
+            </div>
+            <div class="input-group">
+                <label>Confirm Password</label>
+                <input type="password" name="password_2">
+            </div>
+            <div class="input-group">
+                <button type="submit" name="register" class="btn">Register</button>
+            </div>
         <p>
             Already a member ? <a href="login.php">Sign in</a>
         </p>
