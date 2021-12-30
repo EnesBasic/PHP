fcb8601b260d20a3ca21ad1343e1959ebde3a21e
EnesBasic <ssamss3004@gmail.com>
2021-12-30 10:32:36 +0100

    init

--- vjezba/process.php
+++ vjezba/process.php
@@ -4,7 +4,12 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login Page</title>
+    <meta name="keywords" content="HTML, CSS">
+    <meta name="descritpion" content="..."
+    <meta name="author" content="@Enes Basic">
+    <meta name="robots" content="nofollow">
+    <title>Login form</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
     
