316b31639e06d2a4e4c60c0cf7d78679e5324308
EnesBasic <ssamss3004@gmail.com>
2022-01-02 00:50:58 +0100

    init

--- 3012vjezbe/login/reg.clas.php
+++ 3012vjezbe/login/reg.clas.php
@@ -5,6 +5,25 @@ $korisnik="root";
 $password="";
 $db="registar";
 
-$data=mysql_connect($host,$korisnik,$password,$db);
+
+$data=mysqli_connect($host,$korisnik,$password,$db);
+
+if(isset($_POST['register']))
+{
+    $username=$_POST['register'];
+    $email=$_POST['email'];
+    $pass=$_POST['password'];
+
+    $sql="INSERT INTO korisnik(name, email, password) VALUES
+    ('$username', '$email', '$pass')";
+
+    $result=mysqli_query($data, $sql);
+
+    if($result)
+    {
+        echo "register success";
+    }
+}
+
 
 ?>
