e72725fd1a7bd966233d22a94eee19da68dd0709
EnesBasic <ssamss3004@gmail.com>
2022-01-01 23:40:40 +0100

    init

--- 3012vjezbe/login/login.php
+++ 3012vjezbe/login/login.php
@@ -8,9 +8,32 @@
     <meta name="descritpion" content="..."
     <meta name="author" content="@Enes Basic">
     <meta name="robots" content="nofollow">
-    <title>Login form</title>
+    <title>Register page</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
 </head>
 <body>
-    
+    <div class="form_pos";>
+    <h1 class="title">Register</h1>
+        <form class="forma_deg" action="" method="POST">
+            <div>
+                <label class="label">Name</label>
+                <input class="input" type="text" name="name">
+            </div>
+
+            <div>
+                <label class="label">Email</label>
+                <input class="input" type="text" name="email">
+            </div>
+
+            <div>
+                <label class="label">Password</label>
+                <input class="input" type="password" name="password">
+            </div><br>
+
+            <div>
+                <button type="submit" name="register" class="btn">Register</button>
+            </div>
+        </form>
+    </div>
 </body>
 </html>
