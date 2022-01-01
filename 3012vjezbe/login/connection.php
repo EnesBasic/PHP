28adc1426fb3b24df245a8dbe2a724a5f69a0427
EnesBasic <ssamss3004@gmail.com>
2022-01-01 20:45:59 +0100

    init

--- 3012vjezbe/login/connection.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$con = mysqli_connect("localhost", "root","","users");
-if(!$con){
-    die("PLease check your connection". mysqli_error()); 
-}
-
-?>
---
+++ 3012vjezbe/login/login.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="keywords" content="HTML, CSS">
+    <meta name="descritpion" content="..."
+    <meta name="author" content="@Enes Basic">
+    <meta name="robots" content="nofollow">
+    <title>Login form</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body style="background:#CCC;">
+    <div class="header">
+        <h2>Login</h2>
+    </div>
+        <form method="POST" action="process.php">
+            <div class="input-group">
+                <label>username</label>
+                <input type="text" name="username" placeholder="Input here your username...">
+            </div>
+            <div class="input-group">
+                <label>password</label>
+                <input type="password" name="password" placeholder="Input here your password...">
+            </div>
+            <br>
+            <div class="input-group">
+                <button type="submit" name="login" class="btn">Login</button>
+            </div>
+            <p>
+                Not yet a member ?   <a href="register.php">Sign up</a>
+            </p>       
+        </form>
+</body>
+</html>
