2cdace67bd50cdbe5b2bf90ee7557e0dd3a9594f
EnesBasic <ssamss3004@gmail.com>
2022-01-01 21:35:47 +0100

    init

---
+++ 3012vjezbe/login/adminhome.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="keywords" content="HTML, CSS">
+    <meta name="descritpion" content="..."
+    <meta name="author" content="@Enes Basic">
+    <meta name="robots" content="nofollow">
+    <title>Login form</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+    <h1>This is Admin home page</h1>
+</body>
+</html>
--- 3012vjezbe/login/login.php
+++ 3012vjezbe/login/login.php
@@ -46,7 +46,7 @@ if($_SERVER["REQUEST_METHOD"]=="POST")
 <!DOCTYPE html>
 <html lang="en">
 <head>
-<meta charset="UTF-8">
+    <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="keywords" content="HTML, CSS">
