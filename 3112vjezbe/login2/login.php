cb01b4c8f11c53995e3f61c4c0e8b029094cc017
EnesBasic <ssamss3004@gmail.com>
2022-01-03 17:39:51 +0100

    init

---
+++ 3112vjezbe/login2/login.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="keywords" content="HTML, CSS">
+    <meta name="descritpion" content="..."
+    <meta name="author" content="@Enes Basic">
+    <meta name="robots" content="nofollow">
+    <title>Login page</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+<div class="form_pos";>
+    <h1 class="title">Login form</h1>
+        <form class="forma_deg" action="log.db.php" method="POST">
+            <div>
+                <label class="label">Username</label>
+                <input class="input" type="text" name="uname">
+            </div>
+
+            <div>
+                <label class="label">Password</label>
+                <input class="input" type="password" name="pass">
+            </div><br>
+
+            <div>
+                <button type="submit" name="login" class="btn">Login</button>
+            </div>
+        </form>
+    </div>
+</body>
+</html>
---
+++ 3112vjezbe/login2/reg.db.php
@@ -0,0 +1,15 @@
+<?php 
+
+$host="localhost";
+$user="root";
+$password="";
+$db="login2";
+
+$data=mysqli_connect($host,$user,$password,$db);
+
+
+
+
+
+
+?>
