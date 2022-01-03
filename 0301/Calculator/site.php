40c2990f9da087647a07561389ca4ff942e5b25e
EnesBasic <ssamss3004@gmail.com>
2022-01-04 00:37:37 +0100

    init

--- 0301/Calculator/site.php
+++ 0301/Calculator/site.php
@@ -1,2 +1,25 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="keywords" content="HTML, CSS">
+    <meta name="descritpion" content="..."
+    <meta name="author" content="@Enes Basic">
+    <meta name="robots" content="nofollow">
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <title>Calculator</title>
+</head>
+<body>
+    <form action="site.php" method="GET">
+        Name: <input type="text" name="username">
+        <input type="submit" value="submit">
+    </form>
+    <br>
 
+    <?php
+    echo $_GET["username"];
+    ?>
+
+</body>
+</html>
