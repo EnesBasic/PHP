da0b587d982cb2c06e7253988aa37c0c970c3325
EnesBasic <ssamss3004@gmail.com>
2022-01-03 22:54:34 +0100

    init

--- 0301/PHP/index.php
+++ 0301/PHP/index.php
@@ -1,16 +1,30 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="keywords" content="HTML, CSS">
-    <meta name="descritpion" content="..."
-    <meta name="author" content="@Enes Basic">
-    <meta name="robots" content="nofollow">
-    <link rel="stylesheet" type="text/css" href="style.css">
-    <title>HOME page</title>
-</head>
-<body>
+<?php
 
-</body>
-</html>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
