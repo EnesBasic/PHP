e857ae775a8418f105881ae87dd3d41a6fb5840a
EnesBasic <ssamss3004@gmail.com>
2022-01-03 15:58:02 +0100

    init

--- 3112vjezbe/login2/reg.db.php
+++ 3112vjezbe/login2/reg.db.php
@@ -1,13 +1,26 @@
 <?php
 
 $host="localhost";
-$user="root";
+$user ="root";
 $password="";
 $db="registar";
 
-
 $data=mysqli_connect($host, $user, $password, $db);
 
+if(isset($_POST["register"]))
+{
+    $username=$_POST['uname'];
+    $email=$_POST['email'];
+    $password=$_POST['password'];
 
+    $sql="INSERT INTO korisnik (name, email, password) VALUES('$username','$email','$password')";
+
+    $rezult=mysqli_query($data, $sql);
+
+    if($rezult)
+    {
+        echo "register Success!!";
+    }
+}
 
 ?>
