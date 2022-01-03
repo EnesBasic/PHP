237c9beba98093e22e1c071223b66e5b4044f404
EnesBasic <ssamss3004@gmail.com>
2022-01-03 23:09:32 +0100

    init

---
+++ 0301/PHP/includes/person.inc.php
@@ -0,0 +1,16 @@
+<?php
+
+class Person {
+
+    private $first = "Enes";
+    private $last = "Basic";
+    private $age = "42";
+}
+
+class Pet {
+
+    private function owner(){
+        $a = "Hi there!";
+        return $a;
+    }
+}
--- 0301/PHP/index.php
+++ 0301/PHP/index.php
@@ -1,30 +1,22 @@
-<?php
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+<head>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="keywords" content="HTML, CSS">
+    <meta name="descritpion" content="..."
+    <meta name="author" content="@Enes Basic">
+    <meta name="robots" content="nofollow">
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <title>HOME page</title>
+</head>
+<body>
+    <?php
 
+    $pet01 = new Pet();
 
+    echo $pet01->owner();
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-?>
+    ?>
+</body>
+</html>
