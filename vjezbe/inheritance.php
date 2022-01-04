97310ba310492c625b3ff7958de051e4dc64a937
EnesBasic <ssamss3004@gmail.com>
2022-01-04 22:01:25 +0100

    init

--- vjezbe/inheritance.php
+++ vjezbe/inheritance.php
@@ -16,22 +16,36 @@
         ini_set( 'display_errors', 1 );//prikazivanje gresaka xampa na linuxu
      
         class Chef {
-            function makeChicken(){
+            function makeSpecialdish(){
                 echo "The Chef makes chicken <br>"; 
             }
+            
 
             function makeSalad(){
                 echo "The Chef makes salad <br>"; 
             }
 
-            function makeSpecialDish(){
+            function makeSpecialFish(){
                 echo "The Chef makes bbq ribs <br>"; 
             }
             
         }
      
+        class italianChef extends Chef{
+            function makeSpecialDFish(){
+                echo "The ItalianChef makes Pasta<br>";
+            }
+            function makeSpecialDish(){
+                echo "The ItalianChef makes Chicken parm<br>";
+            }
+        }
+
+
         $chef = new Chef();
-        $chef->makeChicken();
+        $chef->makeSpecialDish();
+
+        $italianChef = new italianChef();
+        $italianChef->makeSpecialDish();
         
     ?>
 </body>
