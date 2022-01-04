2a08139ced297a20eb9dbca157ca1d5f936c1369
EnesBasic <ssamss3004@gmail.com>
2022-01-04 18:41:00 +0100

    init

--- vjezbe/inheritance.php
+++ vjezbe/inheritance.php
@@ -8,7 +8,7 @@
     <meta name="author" content="@Enes Basic">
     <meta name="robots" content="nofollow">
     <link rel="stylesheet" type="text/css" href="style.css">
-    <title>object_functions</title>
+    <title>Inheritance/Nasljedivanje</title>
 </head>
 <body>
     <?php
