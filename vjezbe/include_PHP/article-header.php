7e89dcf67c0b5292c530dfd5283267991e598f23
EnesBasic <ssamss3004@gmail.com>
2022-01-04 04:16:08 +0100

    init

---
+++ vjezbe/include_PHP/article-header.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="keywords" content="HTML, CSS">
+    <meta name="descritpion" content="..."
+    <meta name="author" content="@Enes Basic">
+    <meta name="robots" content="nofollow">
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <title>Calculator</title>
+</head>
+<body>
+
+    <h2><?php echo $title; ?></h2>
+
+    <h4><?php echo $author; ?></h4>
+
+    word count : <?php echo $wordCount; ?>
+
+</body>
+</html>
+
+
+
---
+++ vjezbe/include_PHP/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="keywords" content="HTML, CSS">
+    <meta name="descritpion" content="..."
+    <meta name="author" content="@Enes Basic">
+    <meta name="robots" content="nofollow">
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <title>Calculator</title>
+</head>
+<body>
+
+    <?php
+        $title = "My first Post";
+        $author = "Enes";
+        $wordCount = 400;
+         
+        include "article-header.php";
+    ?>
+    
+</body>
+</html>
