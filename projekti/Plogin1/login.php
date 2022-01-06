8ec614a012f2f72310fbc097a109e38d9b1db392
EnesBasic <ssamss3004@gmail.com>
2022-01-06 14:39:23 +0100

    init

--- projekti/Plogin1/login.php
+++ projekti/Plogin1/login.php
@@ -1,6 +1,6 @@
 <?php
 
-if(isset($_POST["username"]) && isset($_POST["password"])){
+if(isset($_POST["username"]) && isset($_POST["passwprd"])){
     if(!empty($_POST["username"]) && !empty($_POST["password"])){
         $username = $_POST["username"];
         $password = $_POST["password"];
