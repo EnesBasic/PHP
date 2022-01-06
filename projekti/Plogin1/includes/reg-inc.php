fc86cb42aa70a6044eecf853348fd0d3eb87c310
EnesBasic <ssamss3004@gmail.com>
2022-01-06 03:05:42 +0100

    init

--- projekti/Plogin1/includes/reg-inc.php
+++ projekti/Plogin1/includes/reg-inc.php
@@ -6,17 +6,20 @@ if(isset($_POST["username"]) && isset($_POST["password"])){
         if(!empty($_POST["username"]) && !empty($_POST["password"])){
            $username = $_POST["username"];
            $password = $_POST["password"];    
-        }                   
 
-        if(file_exists("baza.json")){
-                $useri = file_get_contents("baza.json");
-                $useri = json_decode($useri, true);
-        }else{
-                $useri = array();
-        }
+                if(file_exists("baza.json")){
+                        $useri = file_get_contents("baza.json");
+                        $useri = json_decode($useri, true);
+                }else{$useri = array();}
 
         $useri = array("username" => $username, "password" => $password);
         file_put_contents("baza.json", json_encode("baza.json"));
         header("Location: ../login.php");
-}
+        }else{echo "<span> ERROR Prazno!!!</span>";}}
+
+else{echo "<span> ERROR, Nije poslano***</span>";}
+
+
+
+
 ?>
