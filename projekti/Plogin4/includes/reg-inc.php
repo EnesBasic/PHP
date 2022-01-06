0caba9b285af9f3cc7d2d56f74c1dfaf108ee46c
EnesBasic <ssamss3004@gmail.com>
2022-01-06 02:12:49 +0100

    init

--- projekti/Plogin4/includes/reg-inc.php
+++ projekti/Plogin4/includes/reg-inc.php
@@ -2,21 +2,21 @@
 ini_set('error_reporting', E_ALL);
 ini_set( 'display_errors', 1 );
 
-if(isset($_POST["name"]) && isset($_POST["pas"])){
-    if(!empty($_POST["name"]) && !empty($_POST["pas"])){
-        $uname = $_POST["name"];
-        $pass = $_POST["pas"];
-    }
+//if(isset($_POST["uname"]) && isset($_POST["pass"]));//{
+   // if(!empty($_POST["uname"]) && !empty($_POST["pass"])){
+    //    $uname = $_POST["uname"];
+    //    $pass = $_POST["pass"];
+    //}
 
-    if(file_exists("novi.json")){
-        $kori = file_get_contents("novi.json");
-        $kori = json_decode($kori, true);
-    }else{
-        $kori = array();
-    }
+    //if(file_exists("novi.json")){
+      //  $kori = file_get_contents("novi.json");
+        //$kori = json_decode($kori, true);
+   // }else{
+     //   $kori = array();
+    //}
 
-    $kori = array("name" => $uname, "pas" => $pass);
-    file_put_contents("novi.json", json_encode("novi.json"));
+    //$kori = array("uname" => $uname, "pass" => $pass);
+    //file_put_contents("novi.json", json_encode("novi.json"));
     header("Location: ../log.php");
-}
+//}
 ?>
--- projekti/Plogin4/registracija.php
+++ projekti/Plogin4/registracija.php
@@ -13,10 +13,10 @@
 <body>
     <form action="includes/reg.inc.php" method="POST">
         <label>Username: </label>
-        <input type="text" name="name">
+        <input type="text" name="uname">
         <br>
         <label>Password: </label>
-        <input type="password" name="pas">
+        <input type="password" name="pass">
         <br>
         <input type="submit" name="submit" value="Registruj se">
     </form>
