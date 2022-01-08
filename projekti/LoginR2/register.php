b189b29856bf5ae6aa08ada6d4d766a2558bcb06
EnesBasic <ssamss3004@gmail.com>
2022-01-08 13:05:44 +0100

    init

---
+++ projekti/LoginR2/register.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="keywords" content="HTML, CSS">
+    <meta name="descritpion" content="..."
+    <meta name="author" content="@Enes Basic">
+    <meta name="robots" content="nofollow">
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <title>Register</title>
+</head>
+<body>
+    <form action="" method="post"></form>    
+    <label>Username :</label>
+    <input type="text" name="uname">
+    <br><br>
+    <label>Password</label>
+    <input type="text" name="pass">
+    <br></br>
+    <input type="submit" name="register" value="Register">
+    <br>
+</body>
+</html>
