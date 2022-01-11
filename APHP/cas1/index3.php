d4ec371a6a7bf696fe8d7a1a704ed86d56d02669
EnesBasic <ssamss3004@gmail.com>
2022-01-11 22:19:17 +0100

    init

---
+++ APHP/cas1/index3.php
@@ -0,0 +1,58 @@
+<?php
+
+ini_set('error_reporting', E_ALL);
+ini_set( 'display_errors', 1 );
+
+class Calculator{
+    public $number1;
+    public $number2;
+
+    public function __construct($number1, $number2){
+        $this->number1 = $number1;
+        $this->number2 = $number2;
+    }
+
+    public function add(){
+        echo $this->number1 + $this->number2;
+    }
+
+    public function sub(){
+        echo $this->number1 - $this->number2;
+    }
+
+    public function multiply(){
+        echo $this->number1 * $this->number2;
+    }
+
+    public function div(){
+        echo $this->number1 / $this->number2;
+    }
+}
+
+$calc = new Calculator(4,5);
+
+$calc->$number1 = 7;
+$calc->$number2 = 8;
+
+$calc->add();
+echo "<br>";
+$calc->sub();
+echo "<br>";
+$calc->multiply();
+echo "<br>";
+$calc->div();
+echo "<br>";
+
+echo "<br>";
+
+$calc2 = new Calculator(6,7);
+$calc2->add();
+echo "<br>";
+$calc2->sub();
+echo "<br>";
+$calc2->multiply();
+echo "<br>";
+$calc2->div();
+echo "<br>";
+
+?>
