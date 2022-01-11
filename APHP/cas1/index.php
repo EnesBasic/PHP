2f92e16c25bbf7914c6e52373406e20abc5a9994
EnesBasic <ssamss3004@gmail.com>
2022-01-11 21:46:01 +0100

    init

--- APHP/cas1/index.php
+++ APHP/cas1/index.php
@@ -1,5 +1,8 @@
 <?php
 
+ini_set('error_reporting', E_ALL);
+ini_set( 'display_errors', 1 );
+
 function add($num1, $num2){
     return $num1 + $num2;
 }
@@ -10,7 +13,18 @@ class Claculator{
     }
 }
 
+//echo add(1,2);
+//echo add(2,3);
 
+echo "<br>";
+
+$calc = new Calculator();
+echo $calc->add(1,2);
+
+echo "<br>";
+
+$calc1 = new Calculator();
+echo $calc1->add(2,3);
 
 
 ?>
