7bc82d13a6f96e374042e542a41440b91f9d9390
EnesBasic <ssamss3004@gmail.com>
2022-01-11 22:01:08 +0100

    init

--- APHP/cas1/index2.php
+++ APHP/cas1/index2.php
@@ -7,7 +7,48 @@ class Calculator{
     public $number1;
     public $number2;
 
-    
+    public function __construct($number1, $number2){
+        $this->number1 = $number1;
+        $this->number2 = $number2;
+    }
+
+    public function add(){
+        echo $this->number1 + $this->number2;
+    }
+
+    public function sub(){
+        echo $this->number1 - $this->number2;
+    }
+
+    public function multiply(){
+        echo $this->number1 * $this->number2;
+    }
+
+    public function div(){
+        echo $this->number1 / $this->number2;
+    }
 }
+
+$calc = new Calculator(4,5);
+$calc->add();
+echo "<br>";
+$calc->sub();
+echo "<br>";
+$calc->multiply();
+echo "<br>";
+$calc->div();
+echo "<br>";
+
+echo "<br>";
+
+$calc2 = new Calculator(6,7);
+$calc2->add();
+echo "<br>";
+$calc2->sub();
+echo "<br>";
+$calc2->multiply();
+echo "<br>";
+$calc2->div();
+echo "<br>";
 
 ?>
