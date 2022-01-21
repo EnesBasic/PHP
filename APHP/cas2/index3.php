037023e616b55ef1222a3aca20c49442226b1835
EnesBasic <ssamss3004@gmail.com>
2022-01-21 10:16:43 +0100

    init

--- APHP/cas2/index3.php
+++ APHP/cas2/index3.php
@@ -11,8 +11,32 @@ class TemplateUser{
         $this -> lastname = $lastname;
         $this -> email - $email;
         $this -> password = $password;
+    }
+
+    public function get_Info(){
+        echo "My name is: " .$this->name." ".$this->lastname;
+        echo "<br>";
+        echo "My email is: " .$this->email; 
+        } 
 }
 
+
+
+class User extends TemplateUser{
+    public $status = "2";
+    public function __construct($name, $lastname, $email, $password){
+        parent::__construct($name, $lastname, $email, $password);
+    }
 }
+
+class Admin extends TemplateUser{
+    public $status = "3";
+}
+
+
+$user = new User("Bruce", "Wayne", "bruce@wayne.com", "1234");
+$user->get_Info();
+echo "<br><br>";
+
 
 ?>
