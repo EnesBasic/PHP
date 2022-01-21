edde67e142ca5b28c1d7f3320cf9372a23a985d9
EnesBasic <ssamss3004@gmail.com>
2022-01-21 10:01:50 +0100

    init

---
+++ APHP/cas2/index3.php
@@ -0,0 +1,18 @@
+<?php
+
+class TemplateUser{
+    public $name;
+    public $lastname;
+    public $email;
+    public $password;
+
+    public function __construct($name, $lastname, $email, $password){
+        $this -> name = $name;
+        $this -> lastname = $lastname;
+        $this -> email - $email;
+        $this -> password = $password;
+}
+
+}
+
+?>
