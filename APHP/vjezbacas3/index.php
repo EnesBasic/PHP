b9ef33e388729a47b2dffb58f42092ee6a26fe76
EnesBasic <ssamss3004@gmail.com>
2022-01-25 00:12:03 +0100

    init

--- APHP/vjezbacas3/index.php
+++ APHP/vjezbacas3/index.php
@@ -14,21 +14,36 @@
 <body>
     <div class="form_pos";>
     <h1 class="title">Login form</h1>
-        <form class="forma_deg" action="log.db.php" method="POST">
+        <form class="forma_deg" action="process.php" method="POST">
             <div>
                 <label class="label">Username</label>
-                <input class="input" type="text" name="uname">
+                <input class="input" type="text" name="username" required maxlength="100">
             </div>
 
             <div>
                 <label class="label">Password</label>
-                <input class="input" type="password" name="password">
-            </div><br>
-
+                <input class="input" type="password" name="password" required maxlength="256">
+            </div>      
+            <div>
+                <label class="label">E-mail</label>
+                <input class="input" type="email" name="email" required maxlength="50">
+            </div>
+            <br>
             <div>
                 <button type="submit" name="login" class="btn">Login</button>
             </div>
         </form>
     </div>
+    <?php
+    
+    ini_set('error_reporting', E_ALL);
+    ini_set( 'display_errors', 1 );
+    
+    if(isset($_GET["message"]) && !empty($_GET["message"])){
+        echo "<h2>" .$_GET["message"]. "</h2>";
+    }
+    
+    ?>
+
 </body>
 </html>
