ce70e589f3d275774cd4e56dc7dcff7ff19f9eea
EnesBasic <ssamss3004@gmail.com>
2022-01-25 00:10:34 +0100

    init

---
+++ APHP/vjezbacas3/login3/reg.db.php
@@ -0,0 +1,28 @@
+<?php
+
+$host="localhost";
+$user ="root";
+$password="";
+$db="registar";
+
+
+$data=mysqli_connect($host, $user, $password, $db);
+
+if(isset($_POST["register"]))
+{
+    $username=$_POST['uname'];
+    $email=$_POST['email'];
+    $password=$_POST['password'];
+
+    $sql = "INSERT INTO korisnik (name, email, password) VALUES('$username','$email','$password')";
+
+    $rezult = mysqli_query($data, $sql);
+
+    if($rezult)
+    {
+        header("Location: login.php");
+    }
+
+}
+
+?>
---
+++ APHP/vjezbacas3/login3/register.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="keywords" content="HTML, CSS">
+    <meta name="descritpion" content="..."
+    <meta name="author" content="@Enes Basic">
+    <meta name="robots" content="nofollow">
+    <title>Register page</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+    <div class="form_pos";>
+    <h1 class="title">Register form</h1>
+        <form class="forma_deg" action="reg.db.php" method="POST">
+            <div>
+                <label class="label">Name</label>
+                <input class="input" type="text" name="uname">
+            </div>
+
+            <div>
+                <label class="label">Email</label>
+                <input class="input" type="text" name="email">
+            </div>
+
+            <div>
+                <label class="label">Password</label>
+                <input class="input" type="password" name="password">
+            </div><br>
+
+            <div>
+                <button type="submit" name="register" class="btn">Register</button>
+            </div>
+
+        </form>
+    </div>
+</body>
+</html>
