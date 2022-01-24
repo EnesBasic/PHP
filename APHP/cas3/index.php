36c4348027d805cd3ad32057de0161c021d4174b
EnesBasic <ssamss3004@gmail.com>
2022-01-24 23:27:31 +0100

    init

--- APHP/cas3/index.php
+++ APHP/cas3/index.php
@@ -5,5 +5,10 @@ ini_set('error_reporting', E_ALL);
 ini_set( 'display_errors', 1 );
 
 
+// Pravljenje konekcije na bazu podataka
+$connection = new mysqli("localhost", "root", "");
+if($connection->connect_error){
+    die("Connection failed :" .$connection->connect_error);
+}
 
 ?>
