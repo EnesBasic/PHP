31414ad665a45df82fc0834ef754f6d2d5f590e2
EnesBasic <ssamss3004@gmail.com>
2022-01-24 23:37:54 +0100

    init

--- APHP/cas3/index.php
+++ APHP/cas3/index.php
@@ -6,9 +6,23 @@ ini_set( 'display_errors', 1 );
 
 
 // Pravljenje konekcije na bazu podataka
-$connection = new mysqli("localhost", "root", "");
+$connection = new mysqli("localhost", "root", "", "user");
 if($connection->connect_error){
     die("Connection failed :" .$connection->connect_error);
 }
+
+// Dobavljanje iz baze podataka
+$sql = "SELECT  * FROM user";
+$result = $connection->query($sql);
+
+if($result->num_rows > 0 ){
+    while($row = $result->fetch_assoc()){
+        echo "id: " .$row["id"]. 
+        ", username: " .$row["username"].
+        ", password: " .$row["password"].
+        ", email: " .$row["email"];
+    }
+}
+
 
 ?>
