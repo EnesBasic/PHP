282a8fb2d441b0c023b6774a2e9ff1e8052fb231
EnesBasic <ssamss3004@gmail.com>
2022-01-25 00:53:49 +0100

    init

--- APHP/vjezbacas3/login.php
+++ APHP/vjezbacas3/login.php
@@ -13,24 +13,19 @@
 </head>
 <body>
     <div class="form_pos";>
-    <h1 class="title">Register Form</h1>
-        <form class="forma_deg" action="process.php" method="POST">
-            <div>
-                <label class="label">Username</label>
-                <input class="input" type="text" name="username" required maxlength="100">
-            </div>
-
-            <div>
-                <label class="label">Password</label>
-                <input class="input" type="password" name="password" required maxlength="256">
-            </div>      
+    <h1 class="title">Login Form</h1>
+        <form class="forma_deg" action="login.php" method="POST">
             <div>
                 <label class="label">E-mail</label>
                 <input class="input" type="email" name="email" required maxlength="50">
             </div>
+            <div>
+                <label class="label">Password</label>
+                <input class="input" type="password" name="password" required maxlength="256">
+            </div>      
             <br>
             <div>
-                <button type="submit" name="login" class="btn">Register</button>
+                <button type="submit" name="login" class="btn">Login</button>
             </div>
         </form>
     </div>
