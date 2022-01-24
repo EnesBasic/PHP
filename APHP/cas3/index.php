a81e441cc95e0b5b950a50cd054d74cebb55700d
EnesBasic <ssamss3004@gmail.com>
2022-01-24 23:40:19 +0100

    init

--- APHP/cas3/index.php
+++ APHP/cas3/index.php
@@ -20,7 +20,7 @@ if($result->num_rows > 0 ){
         echo "id: " .$row["id"]. 
         ", username: " .$row["username"].
         ", password: " .$row["password"].
-        ", email: " .$row["email"];
+        ", email: " .$row["email"]. "<br>";
     }
 }
 
