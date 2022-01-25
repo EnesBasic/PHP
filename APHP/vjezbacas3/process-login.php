ba0325660abe8afa5915cbd03d492501d1d402c9
EnesBasic <ssamss3004@gmail.com>
2022-01-25 01:00:30 +0100

    init

--- APHP/vjezbacas3/process-login.php
+++ APHP/vjezbacas3/process-login.php
@@ -6,7 +6,13 @@ ini_set( 'display_errors', 1 );
 
 if(isset($_POST["username"]) && isset($_POST["password"])&& isset($_POST["email"])){
     if(!empty($_POST["username"])&& !empty($_POST["password"])&& !empty($_POST["email"])){
-        
+        $email = $_POST["email"];
+        $password = $_POST["password"];
+
+        if(strlen($email)>50 || strlen($password)>256){
+            header("Location: login.php?message=Jedni polje je predugacko!");
+            
+        }
 
 
     }
