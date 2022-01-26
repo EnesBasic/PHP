3d32f2981396bada37842219c8b069884f9b3858
EnesBasic <ssamss3004@gmail.com>
2022-01-26 22:21:38 +0100

    init

--- APHP/vjezbanje3/register.php
+++ APHP/vjezbanje3/register.php
@@ -1,7 +1,10 @@
 <?php
 
-//ini_set('error_reporting', E_ALL);
-//ini_set( 'display_errors', 1 );
+ini_set('error_reporting', E_ALL);
+ini_set( 'display_errors', 1 );
+
+//include_once "index.php";
+
 
 $servername ="localhost";
 $username ="root";
