217ca143b8070c2880780c79d21df7355229d5eb
EnesBasic <ssamss3004@gmail.com>
2022-01-30 10:17:56 +0100

    init

--- APHP/cas4PDO/index.php
+++ APHP/cas4PDO/index.php
@@ -6,6 +6,20 @@ ini_set ('log_errors', 'on');
 ini_set ('display_startup_errors', 'on');
 ini_set ('error_reporting', E_ALL);
 
+$servername="localhost";
+$username = "root";
+$password = "";
+$database = "ita_app";
+
+
+try{
+    $connection=new PDO("mysql:host=$servername;dbname=$database", $username, $password);
+    $connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Connection established";
+}catch(PDOException $e){
+    echo "Connection failed!".$e->getMessage();
+}
+
 
 
 ?>
