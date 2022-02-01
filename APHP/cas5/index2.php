33739bac658c5a076fe7b441d8a5aa238a20e3e5
EnesBasic <ssamss3004@gmail.com>
2022-02-01 18:23:11 +0100

    init

--- APHP/cas5/index2.php
+++ APHP/cas5/index2.php
@@ -6,5 +6,12 @@ ini_set ('display_startup_errors', 'on');
 ini_set ('error_reporting', E_ALL);
 
 
+require "connection.class.php";
+require "database.class.pphp";
+require "invoice.class.php";
+require "project.class.php";
+require "user.class.php";
+
+$connection  = new Database\Connection\Connection();
 
 ?>
