4465f82d09c3917e1c394b0441c5ef5a25700510
EnesBasic <ssamss3004@gmail.com>
2022-02-07 21:58:49 +0100

    init

--- APHP/vjezbacas4/index.php
+++ APHP/vjezbacas4/index.php
@@ -15,7 +15,11 @@ require "connection.php";
     <meta name="author" content="@Enes Basic">
     <meta name="robots" content="nofollow">
     <title>Dashboard</title>
-    <link rel="stylesheet" type="text/css" href="style.css">
+    <style>
+        table, tr, td, th{
+            border:1px solid #000;
+            border-collapse:collapse;
+        }
 </head>
 <body>
     <?php
@@ -32,50 +36,38 @@ require "connection.php";
     
     ?>
 
-    <table>
-        <thead>
-            <tr>
-                <th>ID</th>
-                <th>NAME</th>
-                <th>PASSWORD</th>
-                <th>DATE AND TIME</th>
-                <th>STATUS</th>
-            </tr>
-        </thead>
-        <tbody>
-            <?php
-            
-            foreach($results as $column => $value){
-                echo "<tr>";
-                echo "<td>" .$value["id"]. "</td>";
-                echo "<td>" .$value["name"]. "</td>";
-                echo "<td>" .$value["password"]. "</td>";
-                echo "<td>" .$value["datetime"]. "</td>";
+        <table>
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>NAME</th>
+                    <th>PASSWORD</th>
+                    <th>DATE AND TIME</th>
+                    <th>STATUS</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php
+                
+                foreach($results as $column => $value){
+                    echo "<tr>";
+                    echo "<td>" .$value["id"]. "</td>";
+                    echo "<td>" .$value["name"]. "</td>";
+                    echo "<td>" .$value["password"]. "</td>";
+                    echo "<td>" .$value["datetime"]. "</td>";
 
-                if($value["status"]=="1"){
-                    echo "<td> Aktivan </td>";
-                }else{
-                    
+                    if($value["status"]=="1"){
+                        echo "<td> Aktivan </td>";
+                    }else{
+                        echo "<td> NeAktivan </td>"; 
+                    }
                 }
 
-            }
-            
-            ?>
-        </tbody>
-
-
-    </table>
-
-    <?php
-    
-    ini_set('error_reporting', E_ALL);
-    ini_set( 'display_errors', 1 );
-    
-    if(isset($_GET["message"]) && !empty($_GET["message"])){
-        echo "<h2>" .$_GET["message"]. "</h2>";
-    }
-    
     ?>
+            </tbody>
+        </table>
+
+  
 
 </body>
 </html>
@@ -86,56 +78,4 @@ require "connection.php";
 
 
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="keywords" content="HTML, CSS">
-    <meta name="descritpion" content="..."
-    <meta name="author" content="@Enes Basic">
-    <meta name="robots" content="nofollow">
-    <title>Dashboard</title>
-    <link rel="stylesheet" type="text/css" href="style.css">
-</head>
-<body>
-    <div class="form_pos";>
-    <h1 class="title">Register Form</h1>
-        <form class="forma_deg" action="process.php" method="POST">
-            <div>
-                <label class="label">Username</label>
-                <input class="input" type="text" name="username" required maxlength="100">
-            </div>
 
-            <div>
-                <label class="label">Password</label>
-                <input class="input" type="password" name="password" required maxlength="256">
-            </div>      
-            <div>
-                <label class="label">E-mail</label>
-                <input class="input" type="email" name="email" required maxlength="50">
-            </div>
-            <br>
-            <div>
-                <button type="submit" name="login" class="btn">Register</button>
-            </div>
-        </form>
-    </div>
-    <?php
-    
-    ini_set('error_reporting', E_ALL);
-    ini_set( 'display_errors', 1 );
-    
-    if(isset($_GET["message"]) && !empty($_GET["message"])){
-        echo "<h2>" .$_GET["message"]. "</h2>";
-    }
-    
-    ?>
-
-</body>
-</html>
-
-
-
-*/
