01c485def6c7d7aa884afe0dd0839ed9c2aa165e
EnesBasic <ssamss3004@gmail.com>
2022-02-08 21:52:46 +0100

    init

--- APHP/vjezbacas4/index.php
+++ APHP/vjezbacas4/index.php
@@ -11,15 +11,38 @@ $password = "";
 $database = "ita_app";
 
 try{
-    $conn = new PDO("mysql:host=$servername;dbname=$database", $username, $password);
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $conn = new PDO ("mysql:host=$servername; dbname=$database", $username, $password);
+    $conn->setAttribute (PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     echo "Connection established";
     $sql = $conn-> prepare("SELECT * FROM user");
-    $sql->execute();
+    $sql-> execute();
 
-    $result = $sql->setFechMode(PDO::FETCH_ASSOC);
+    $result = $sql->setFetchMode(PDO::FETCH_ASSOC);
     $results = $sql->fetchAll();
-    var_dump($results);
+ 
+    echo "<ul>";
+    foreach ($results as $column => $value) {
+        echo "<li>" .$value["name"]. "</li>";
+        echo "<li>" .$value["password"]. "</li>";
+        echo "<li>" .$value["datetime"]. "</li>";
+        if($value["status"]==1){
+            echo "<li> Aktivan korisnik </li>";
+            //header("Location:dashboard.php");
+        }else{
+            echo "<li> Ne Aktivan korisnik </li>";
+            //header("Location:inactive.php");
+        }
+        echo "<li><br></li>";
+    }
+    echo "</ul>";
+
+    $date = date("H:i:s d.m.Y");
+
+    $sql_insert = "INSERT INTO user 
+    (name, password, datetime, status)
+    VALUES ('Rampage', '5678', '{$date}', '1')";
+
+    $conn->exec($sql_insert);
 
 }catch(PDOException $e){
     echo "connection error: " .$e->getMessage();
