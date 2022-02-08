c498d6414ae6224d4263014c8d6c39646141a685
EnesBasic <ssamss3004@gmail.com>
2022-02-08 22:39:47 +0100

    init

--- APHP/vjezbacas4/index.php
+++ APHP/vjezbacas4/index.php
@@ -36,13 +36,55 @@ try{
     }
     echo "</ul>";
 
+    // Kako dobiti automatski datum i vrijeme
     $date = date("H:i:s d.m.Y");
 
+    //Unos samih podataka
     $sql_insert = "INSERT INTO user 
     (name, password, datetime, status)
     VALUES ('Rampage', '5678', '{$date}', '1')";
 
+    //Prikaz posljednje unijetog ID-a (ovo je vezno zatrenutnu konekciju )
+    //Dolazi neposredno poslije Inserta(moze biti funkcionalnosti izmedu ali ne sa bazom ako hoce)mo da funckcionise)
+    echo "<p>" .$last_insert_id= $conn->lastInsertId(). "</p>";
+
+    //Izvrsavanje samog querija
     $conn->exec($sql_insert);
+    echo "<p>Nesto trece...</p>";
+
+    // Updateovanje podataka unutar tabele
+
+    // Formiranje update datuma
+    $date_update = date("H:i:s d.m.Y");
+
+    // kreiranje upita
+    $sql_update = "UPDATE user SET name='Rampage', datetime='{$date_update}' WHERE name='Dell'";
+    
+    //Prirpema upita
+    $result = $conn->prepare($sql_update);
+    
+    // Izvrsavanje upita (execute moze da vrati neki rezultat, dok exec ne moze)
+    $result -> execute();
+    
+    //Opcionalno ispis broja redova koji su se promijenili koristeci ovaj upit iznad
+    echo $result-> rowCount(). "records updated!";
+
+    //Brisanje samih podataka (OBAVEZNO WHERE)
+    //Delete se uvijek odvija nad redovima
+    //Ako biste htjeli jednu kolonu iskoristili bismo (npr SET name='')
+    // Formiramo SQL upit za brisanje
+    $sql_delete= "DELETE FROM user WHERE name='Rampage'";
+    
+    //Brisanje samih redova
+    $conn->exec($sql_delete);
+
+
+
+
+
+
+
+
 
 }catch(PDOException $e){
     echo "connection error: " .$e->getMessage();
