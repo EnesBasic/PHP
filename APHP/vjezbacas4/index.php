7cf302c9ec5fee67765c72a62fbe5bf33fe87f6b
EnesBasic <ssamss3004@gmail.com>
2022-02-08 21:09:14 +0100

    init

--- APHP/vjezbacas4/index.php
+++ APHP/vjezbacas4/index.php
@@ -1,8 +1,8 @@
 <?php
 
-ini_set ('display_errors', 'on');
-ini_set ('log_errors', 'on');
-ini_set ('display_startup_errors', 'on');
+ini_set ('display_errors', TRUE);
+ini_set ('log_errors', TRUE);
+ini_set ('display_startup_errors', TRUE);
 ini_set ('error_reporting', E_ALL);
 
 $servername = "localhost";
@@ -11,34 +11,19 @@ $password = "";
 $database = "ita_app";
 
 try{
-    $connection = new PDO("mysql:host=$servername;dbname=$database", $username, $password);
-    $connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $conn = new PDO("mysql:host=$servername;dbname=$database", $username, $password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     echo "Connection established";
-    $sql = $connection->prepare("SELECT * FROM user");
+    $sql = $conn-> prepare("SELECT * FROM user");
     $sql->execute();
-    
+
     $result = $sql->setFechMode(PDO::FETCH_ASSOC);
     $results = $sql->fetchAll();
-
-    echo "<ul>";
-    foreach($results as $column => $value){
-        echo "<li>";
-        echo "<li>" .$value["id"]. "</li>";
-        echo "<li>" .$value["name"]. "</li>";
-        echo "<li>" .$value["password"]. "</li>";
-        echo "<li>" .$value["datetime"]. "</li>";
-
-        if($value["status"]=="1"){
-            echo "<li> Aktivan </li>";
-        }else{
-            echo "<li> NeAktivan </li>"; 
-        }
-    }
-    echo "</ul>";
+    var_dump($results);
 
 }catch(PDOException $e){
     echo "connection error: " .$e->getMessage();
 }
 
-$connection= null;
+$conn = null;
 ?>
