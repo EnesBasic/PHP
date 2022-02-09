498fc9ae50ffae3fb5c4aa052fe967a2f91b3b73
EnesBasic <ssamss3004@gmail.com>
2022-02-09 01:37:37 +0100

    init

---
+++ APHP/vjezbacas4/insert.function.php
@@ -0,0 +1,42 @@
+<?php
+
+require "connection.php";
+
+if(isset($_POST["name"]) && isset($_POST["password"]) && isset($_POST["status"])){
+    if(!empty($_POST["name"]) && !empty($_POST["password"]) && !empty($_POST["status"])){
+        $name = $_POST["name"];
+        $password = $_POST["password"];
+        $status = $_POST["status"];
+        $datetime = date("H:i:s d.m.Y");
+
+        $name = trim($name);
+        $password = trim($password);
+        $status = trim($status);
+        $password = md5($password);
+
+        $sql_insert = "INSERT INTO user
+        (name, password, status, datetime)
+        VALUES
+        ('{$name}', '{$password}', '{$status}', '{$datetime}')";
+
+        $connection->exec($sql_insert);
+        //Uraditi provjeru da li je izvresen Unos
+        if($connection->lastInsertId() > 0){
+            header("Location: index.php");
+            echo "Uspjesno ste unijeli novog korisnika";
+            //Vratiti poruku o uspjesnosti
+        }else{
+            header("Location: index.php");
+            echo "Greska pri unosu";
+            //vratiti poruku o NEuspjesnosti
+        }
+
+    }else{
+        //vratiti gresku
+    }
+
+}else{
+    //vratiti gresku
+}
+
+?>
