abd15eba0f7b8c8c6f6e8d0d1bd1bbd2a067db32
EnesBasic <ssamss3004@gmail.com>
2022-03-28 22:04:30 +0200

    init

--- APHP/cas5.6 namespace/index.php
+++ APHP/cas5.6 namespace/index.php
@@ -1,11 +1,15 @@
 <?php
 
-ini_set ('display_errors', 'on');
-ini_set ('log_errors', 'on');
-ini_set ('display_startup_errors', 'on');
-ini_set ('error_reporting', E_ALL);
+/**ini_set ('display_errors', 'on');
+*ini_set ('log_errors', 'on');
+*ini_set ('display_startup_errors', 'on');
+*ini_set ('error_reporting', E_ALL);
+*/
 
 use Database\Connection as DC;
+use Database as DB;
+use Project as PR;
+
 
 require "Connection.class.php";
 require "Database.class.php";
@@ -14,11 +18,11 @@ require "Project.class.php";
 require "User.class.php";
 
 
-$connection = new D\Connection();
-$database = new Database\Database();
-$invoice = new Project\Invoice();
-$project = new Project\Project();
-$user = new Project\User();
+$connection = new DC\Connection();
+$database = new DB\Database();
+$invoice = new PR\Invoice();
+$project = new PR\Project();
+$user = new PR\User();
 
 
 ?>
