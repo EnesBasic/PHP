535b79c400a4678636865c1321d8b264a624bb7b
EnesBasic <ssamss3004@gmail.com>
2022-03-29 14:33:29 +0200

    init

--- APHP/cas5.9 singletone2/index.php
+++ APHP/cas5.9 singletone2/index.php
@@ -1,5 +1,13 @@
 <?php
 
+ini_set ('display_errors', 'on');
+ini_set ('log_errors', 'on');
+ini_set ('display_startup_errors', 'on');
+ini_set ('error_reporting', E_ALL);
+
+
+
+
 class Connection{
     private static $instance;
 
@@ -14,21 +22,33 @@ class Connection{
     }
 
     public static function connect(){
+        $connection = self::getInstance();
 
         $hostname = "localhost";
         $user = "root";
         $password = "";
         $database = "user";
 
-        $connection = new mysqli($hostname, $user, $password, $database);
+        $connection->connection = new mysqli($hostname, $user, $password, $database);
+        //ili
+        //$connection = new mysqli($hostname, $user, $password, $database);
         return $connection;
-        
+
+    }
+
+    public function __clone(){
+        throw new Exception("Can't clone a singleton databse connection class");
     }
 }
 
 $a = Connection::getInstance();
 $b = Connection::getInstance();
 
+$c = clone $b;
+
+$connection = $a::connect();
 var_dump($a === $b);
+//var_dump($connection);
+var_dump($connection->$connection);
 
 ?>
