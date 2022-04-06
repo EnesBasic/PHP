00cf9979ab04bb1a062a8afa7173ab283b222562
Enes Basic <ssamss3004@gmail.com>
2022-04-06 22:24:11 +0200

    init

--- APHP/cas5.1/user.php
+++ APHP/cas5.1/user.php
@@ -1,14 +1,15 @@
 <?php
 
-ini_set ('display_errors', 'on');
-ini_set ('log_errors', 'on');
-ini_set ('display_startup_errors', 'on');
-ini_set ('error_reporting', E_ALL);
+//ini_set ('display_errors', 'on');
+//ini_set ('log_errors', 'on');
+//ini_set ('display_startup_errors', 'on');
+//ini_set ('error_reporting', E_ALL);
 
-
-class user{
-    echo "<h1> Pozdrav svima</h1>";
+class User{
+    
+    public function __construct(){
+    echo "<h1> Pozdrav svima! </h1>";
+    }
 }
-
 
 ?>
