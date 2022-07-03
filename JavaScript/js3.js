0436bb99852febb62e08010572aae38e38f7a28f
Enes Basic <ssamss3004@gmail.com>
2022-07-03 20:50:18 +0200

    init

--- JavaScript/js3.js
+++ JavaScript/js3.js
@@ -8,5 +8,17 @@ students /= 4; //students = students / 4
 students %= 2; //students = students % 2 
 
 
+/*
+        operator presedence
+
+1. parenthesis ()
+2. exponents
+3. multiplication & division
+4. addition & substraction 
+
+*/
+let result = 1 + 2 * ( 3 + 4);
+
 console.log(students);
+console.log(result);
 
