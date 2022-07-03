ebd1edd6c1d02ff3a6f690c34e795b4bb89db864
Enes Basic <ssamss3004@gmail.com>
2022-07-03 20:22:59 +0200

    init

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -1,4 +1,23 @@
 
-console.log("Merhaba World"); //semicolon is optional
+//console.log("Hello World"); //semicolon is optional
 
-window.alert("Merhaba Dzihan")
+//window.alert("Hello student")
+
+/*
+This
+is 
+multiline
+comment
+*/
+
+//
+let firstname = "Enes"; // strings
+let age = 40; // numbers
+age = age + 2;
+let student = true; //boolean
+
+
+console.log("Hello", firstname);
+console.log("You are", age, "years old");
+console.log(student);
+
