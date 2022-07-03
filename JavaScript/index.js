3c184f64401569762e00f567f5f60f7e01b42e05
Enes Basic <ssamss3004@gmail.com>
2022-07-03 20:27:54 +0200

    init

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -19,5 +19,9 @@ let student = true; //boolean
 
 console.log("Hello", firstname);
 console.log("You are", age, "years old");
-console.log(student);
+console.log("Enrolled :", student);
+
+document.getElementById("p1").innerHTML="Hello " + firstname;
+document.getElementById("p2").innerHTML="You are " + age + " years old";
+document.getElementById("p3").innerHTML="Enrolled :" + student;
 
